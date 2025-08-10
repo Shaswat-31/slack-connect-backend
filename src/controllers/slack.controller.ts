dd210c4f9b862f50751f09a9cf51391782aa1ee3
Shaswat-31 <143761992+Shaswat-31@users.noreply.github.com>
2025-08-10 14:58:05 +0530

    user id passed in params

--- src/controllers/slack.controller.ts
+++ src/controllers/slack.controller.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 export const slackCallback = async (req: Request, res: Response) => {
   const code = req.query.code as string;
-  const userId = req.user?.id; // pass state param in step 1 if you want to know which user
+  const userId = req.query.userId as string; 
 
   if(!userId){
     return res.status(500).json({ error: 'OAuth failed because user id missing' });
@@ -55,10 +55,11 @@ export const slackCallback = async (req: Request, res: Response) => {
 export const slackConnect=async(req:Request, res:Response) => {
   console.log("slack connect");
   const clientId = process.env.SLACK_CLIENT_ID;
-  const redirectUri = encodeURIComponent(`${process.env.BACKEND_URL}/api/slack/callback`);
+   const userId = req.user?.id; 
+    const state = encodeURIComponent(userId as string);
+  const redirectUri = encodeURIComponent(`${process.env.BACKEND_URL}/api/slack/callback?userId=${userId}`);
   const scope = encodeURIComponent('channels:read chat:write');
-
   const url = `https://slack.com/oauth/v2/authorize?client_id=${clientId}&scope=${scope}&redirect_uri=${redirectUri}`;
-  const anUrl='https://slack.com/oauth/v2/authorize?client_id=339171545393.9352700732976&scope=chat:write,channels:read,groups:read,channels:join&redirect_uri=https://oauth.pstmn.io/v1/callback'
+  //const anUrl='https://slack.com/oauth/v2/authorize?client_id=339171545393.9352700732976&scope=chat:write,channels:read,groups:read,channels:join&redirect_uri=https://oauth.pstmn.io/v1/callback'
   res.json({ redirectUrl: url });
 }
